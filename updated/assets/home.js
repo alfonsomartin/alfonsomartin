9e24b877bb5533a970656f6495c96a08698ad78d
amartin3 <alfonsomartin344@gmail.com>
2018-01-13 11:15:50 -0800

    updates, automatic

--- updated/assets/home.js
+++ updated/assets/home.js
@@ -1,22 +1,18 @@
-// $(document).ready(function(){
+var imgArray = [];
+var theHeight = $(window).height() - 75;
+var theWidth = $(window).width() - 75;
 
-// // random bubble size
-// var colors = [
-//         "rgba(186, 92, 186, 0.7)",
-//         "rgba(131,224,224,0.7)",
-//         "rgba(13, 199, 165, 0.7)",
-//         "rgba(86, 60, 232, 0.7)",
-//         "rgba(242, 206, 233, 0.7)",
-//         "rgba(117, 238, 137, 0.7)",
-//         "rgba(213, 236, 158, 0.7)"
-//     ];
+$(document).ready(function(e) {
+    $("img.draggable").each(function(index, element) {
 
-//     $('.project:hover').each(function() {
+        var tLeft = Math.floor(Math.random()*theWidth);
+        var tTop  = Math.floor(Math.random()*theHeight);
 
-//         var random = Math.floor(Math.random() * (max - min + 1)) + min;
+            $(element).css({position:'absolute', left: tLeft, top: tTop});
+            $(element).draggable();     
 
-//         var randomColor = colors[Math.floor(Math.random()*colors.length)];
-//         console.log("random color is " + randomColor);
-        
-//         $(this).css("box-shodow: inset 0 0 0 1000px", randomColor);
-//  });
+    });
+});  
+
+
+$( ".draggable" ).draggable({ containment: "#containment-wrapper", scroll: false })
