e05b7c4c060971eea6234942115f228a8db29935
amartin3 <alfonsomartin344@gmail.com>
2019-03-06 17:34:46 -0800

    About Me

--- assets/home.js
+++ assets/home.js
@@ -1,6 +1,6 @@
 var imgArray = [];
-var theHeight = $(window).height() - 75;
-var theWidth = $(window).width() - 75;
+var theHeight = 350;
+var theWidth = 350;
 
 $(document).ready(function(e) {
     $("img.draggable").each(function(index, element) {
@@ -16,3 +16,7 @@ $(document).ready(function(e) {
 
 
 $( ".draggable" ).draggable({ containment: "#containment-wrapper", scroll: false })
+
+var colors = ['#d4d97e', '#ae529f', '#f28e29', '#539a9f', '#f6b2c1', '#c0b5d4'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.getElementById('con-2').style.backgroundColor = random_color;
